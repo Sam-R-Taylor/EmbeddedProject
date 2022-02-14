22f43625d8326add021c2dc8aaba93fa5806bf11
Sam-R-Taylor <59895743+Sam-R-Taylor@users.noreply.github.com>
2022-02-14 18:43:10 +0000

    tried Mqtt

--- lib/views/about/about_view.dart
+++ lib/views/about/about_view.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:the_basics/views/mqtt/mqttView.dart';
+import 'package:the_basics/mqtt/state/MQTTAppState.dart';
+import 'package:provider/provider.dart';
 
 class AboutView extends StatelessWidget {
   const AboutView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-    );
+    return MaterialApp(
+        title: 'View',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: ChangeNotifierProvider<MQTTAppState>(
+          create: (_) => MQTTAppState(),
+          child: MQTTView(),
+        ));
   }
 }
