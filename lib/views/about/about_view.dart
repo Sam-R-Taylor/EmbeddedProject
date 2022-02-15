3da2e7b7e707a36197b768a916cac82119a159a9
Sam-R-Taylor <59895743+Sam-R-Taylor@users.noreply.github.com>
2022-02-15 17:52:49 +0000

    Nearly Working

--- lib/views/about/about_view.dart
+++ lib/views/about/about_view.dart
@@ -1,69 +1,177 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
-import 'package:the_basics/env.sample.dart';
-import 'package:the_basics/model/Water.dart';
+import 'dart:async';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:the_basics/widgets/call_to_action/call_to_action.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_browser_client.dart';
 
 class AboutView extends StatefulWidget {
+  AboutView({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
-  AboutViewState createState() => AboutViewState();
+  _AboutViewState createState() => _AboutViewState();
 }
 
-class AboutViewState extends State<AboutView> {
-  Future<List<Aqua>> Aquas;
-  final AquaListKey = GlobalKey<AboutViewState>();
+class _AboutViewState extends State<AboutView> {
+  String broker = "ws://127.0.0.1";
+  int port = 1883;
+  String username = '';
+  String passwd = '';
+  String clientIdentifier = 'SamApp';
 
-  @override
-  void initState() {
-    super.initState();
-    Aquas = getAquaList();
-  }
+  MqttBrowserClient client;
+  MqttConnectionState connectionState;
 
-  Future<List<Aqua>> getAquaList() async {
-    final response =
-        await http.get(Uri.parse("${Env.URL_PREFIX}/employeedetails"));
+  double _temp = 20;
 
-    final items = json.decode(response.body).cast<Map<String, dynamic>>();
-    List<Aqua> Aquas = items.map<Aqua>((json) {
-      return Aqua.fromJson(json);
-    }).toList();
+  StreamSubscription subscription;
 
-    return Aquas;
+  void _subscribeToTopic(String topic) {
+    if (connectionState == MqttConnectionState.connected) {
+      print('[MQTT client] Subscribing to ${topic.trim()}');
+      client.subscribe(topic, MqttQos.exactlyOnce);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      key: AquaListKey,
       appBar: AppBar(
-        title: Text('Aqua List'),
+        // Here we take the value from the AboutView object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('MQTT Messages'),
       ),
       body: Center(
-        child: FutureBuilder<List<Aqua>>(
-          future: Aquas,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            // By default, show a loading spinner.
-            if (!snapshot.hasData) return CircularProgressIndicator();
-            // Render Aqua lists
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int index) {
-                var data = snapshot.data[index];
-                return Card(
-                  child: ListTile(
-                    leading: Icon(Icons.person),
-                    title: Text(
-                      data.ename,
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                );
-              },
-            );
-          },
+        child: Container(
+          child: ListTile(
+            title: Text(_temp.toString()),
+            tileColor: Color.fromARGB(255, 255, 255, 255),
+          ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _connect,
+        tooltip: 'Play',
+        child: Icon(Icons.play_arrow),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _connect() async {
+    /// First create a client, the client is constructed with a broker name, client identifier
+    /// and port if needed. The client identifier (short ClientId) is an identifier of each MQTT
+    /// client connecting to a MQTT broker. As the word identifier already suggests, it should be unique per broker.
+    /// The broker uses it for identifying the client and the current state of the client. If you don’t need a state
+    /// to be hold by the broker, in MQTT 3.1.1 you can set an empty ClientId, which results in a connection without any state.
+    /// A condition is that clean session connect flag is true, otherwise the connection will be rejected.
+    /// The client identifier can be a maximum length of 23 characters. If a port is not specified the standard port
+    /// of 1883 is used.
+    /// If you want to use websockets rather than TCP see below.
+    ///
+    client = MqttBrowserClient(broker, '');
+    client.port = port;
+    client.websocketProtocols = MqttClientConstants.protocolsSingleDefault;
+
+    /// A websocket URL must start with ws:// or wss:// or Dart will throw an exception, consult your websocket MQTT broker
+    /// for details.
+    /// To use websockets add the following lines -:
+    /// client.useWebSocket = true;
+    /// client.port = 80;  ( or whatever your WS port is)
+    /// Note do not set the secure flag if you are using wss, the secure flags is for TCP sockets only.
+    /// Set logging on if needed, defaults to off
+    client.logging(on: true);
+
+    /// If you intend to use a keep alive value in your connect message that is not the default(60s)
+    /// you must set it here
+    client.keepAlivePeriod = 30;
+
+    /// Add the unsolicited disconnection callback
+    client.onDisconnected = _onDisconnected;
+
+    /// Create a connection message to use or use the default one. The default one sets the
+    /// client identifier, any supplied username/password, the default keepalive interval(60s)
+    /// and clean session, an example of a specific one below.
+    final MqttConnectMessage connMess = MqttConnectMessage()
+        .withClientIdentifier(clientIdentifier)
+        .startClean() // Non persistent session for testing
+        .keepAliveFor(30)
+        .withWillQos(MqttQos.atMostOnce);
+    print('[MQTT client] MQTT client connecting....');
+    client.connectionMessage = connMess;
+
+    /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
+    /// in some circumstances the broker will just disconnect us, see the spec about this, we however will
+    /// never send malformed messages.
+    try {
+      await client.connect(username, passwd);
+    } catch (e) {
+      print(e);
+      _disconnect();
+    }
+
+    /// Check if we are connected
+    if (client.connectionStatus == MqttConnectionState.connected) {
+      print('[MQTT client] connected');
+      setState(() {
+        connectionState = MqttConnectionState.connected;
+      });
+    } else {
+      print('[MQTT client] ERROR: MQTT client connection failed - '
+          'disconnecting, state is ${client}');
+      _disconnect();
+    }
+
+    /// The client has a change notifier object(see the Observable class) which we then listen to to get
+    /// notifications of published updates to each subscribed topic.
+    subscription = client.updates.listen(_onMessage);
+
+    _subscribeToTopic("samrt");
+  }
+
+  void _disconnect() {
+    print('[MQTT client] _disconnect()');
+    client.disconnect();
+    _onDisconnected();
+  }
+
+  void _onDisconnected() {
+    print('[MQTT client] _onDisconnected');
+    setState(() {
+      //topics.clear();
+      connectionState = MqttConnectionState.disconnected;
+      client = null;
+      subscription.cancel();
+      subscription = null;
+    });
+    print('[MQTT client] MQTT client disconnected');
+  }
+
+  void _onMessage(List<MqttReceivedMessage> event) {
+    print(event.length);
+    final MqttPublishMessage recMess = event[0].payload as MqttPublishMessage;
+    final String message =
+        MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+    /// The above may seem a little convoluted for users only interested in the
+    /// payload, some users however may be interested in the received publish message,
+    /// lets not constrain ourselves yet until the package has been in the wild
+    /// for a while.
+    /// The payload is a byte buffer, this will be specific to the topic
+    print('[MQTT client] MQTT message: topic is <${event[0].topic}>, '
+        'payload is <-- ${message} -->');
+    print(client.connectionStatus);
+    print("[MQTT client] message with topic: ${event[0].topic}");
+    print("[MQTT client] message with message: ${message}");
+    setState(() {
+      _temp = double.parse(message);
+    });
   }
 }
